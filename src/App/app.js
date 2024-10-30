ea2c742049c5b76110bb3f3278d0d8d53031c61c
whats <noamharel02@gmail.com>
2024-10-30 14:03:24 +0200

    Tried to change to App root access

--- src/App/app.js
+++ src/App/app.js
@@ -1,12 +1,13 @@
 import Cli from "../Cli/cli.js";
-import Directory from "../FileSystem/Directory.js";
 import parse from "../Parser/parse.js";
+import Directory from "../FileSystem/Directory.js";
 
 class App {
   cli = new Cli();
 
   async launch() {
     try {
+      this.initializeRoot();
       await this.cli.start(this.readLineHandler);
     } catch (error) {
       console.error(error);
@@ -19,6 +20,10 @@ class App {
     } else {
       parse(line);
     }
+  }
+
+  initializeRoot() {
+    this.root = new Directory("Root", 0);
   }
 }
 
